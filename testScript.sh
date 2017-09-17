3b50d5b6d2c513e849bc68c9f660520eddf27b70
Sasha Spiegelman <sashas@oracle>
2017-09-17 10:36:21 +0300

    minor

--- testScript.sh
+++ testScript.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 
 
-for helpers in {1..10};
-do
-        for levels in {1..6};
-        do
 
-                for writers in 1 2 4 8 16 32;
+                for writers in {1..8};
                 do
-                        lev=$((levels - 1))
-                        leaves=$((2**lev))
-                        if ((leaves >= writers)); then
-#                                echo "############# helpers = $helpers  levels = $levels  writers = $writers  ##################################################################################"
                                 for i in {1..5}; do
-                                #       echo "test"
-					
 					if ((i == 1)); then		
-                                        	java  com.yahoo.sketches.quantiles.TestPerformance $levels $helpers $writers 1 true
+                                        	java  com.yahoo.sketches.quantiles.TestPerformance $writers 30 true
 					else
-						java  com.yahoo.sketches.quantiles.TestPerformance $levels $helpers $writers 1 false
+						java  com.yahoo.sketches.quantiles.TestPerformance $writers 30 false
                                		fi	
 				 done
-                        fi
 
 
                 done
-
-        done
-
-done
 exit 0
